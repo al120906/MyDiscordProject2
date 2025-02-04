5aec189d0ba8e7a8af48a9aa9055cf9b7bb89e22
Alex Grueshaber <15522583-AlexGrueshaber@users.noreply.replit.com>
2025-02-04 03:17:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const mySecret = process.env['TOKEN']
+const express = require("express");
+const app = express();
+const keep_alive = require('./keep_alive.js')
+
+app.listen(3000, () => {
+  console.log("Project is running!");
+})
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+})
+
+const Discord = require("discord.js")
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+client.on("message", message => {
+  if (message.content === "Ping") {
+  message.channel.send ("Pong!")
+    client.user.setActivity("Ping-Pong", { type: "PLAYING"})
+  }
+})
+
+client.login(process.env.TOKEN);
